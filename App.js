0afe0a1fb44630a105163ac0d63017c81515f6fb
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-06-20 14:16:55 +0900

    create memo edit and create screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import MemoListScreen from './src/screens/MemoListScreen.jsx';
-import MemoDetailScreen from './src/screens/MemoDetailScreen.jsx';
+// import MemoListScreen from './src/screens/MemoListScreen.jsx';
+// import MemoDetailScreen from './src/screens/MemoDetailScreen.jsx';
+// import MemoEditScreen from './src/screens/MemoEditScreen.jsx';
+import MemoCreateScreen from './src/screens/MemoCreateScreen.jsx';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
 
