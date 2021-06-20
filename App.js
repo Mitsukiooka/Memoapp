7949f4f8a6e21897f4a351099a2973824f938c44
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-06-20 14:27:26 +0900

    replace keyboardavoidingview

--- App.js
+++ App.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen.jsx';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen.jsx';
-// import MemoEditScreen from './src/screens/MemoEditScreen.jsx';
+import MemoEditScreen from './src/screens/MemoEditScreen.jsx';
 import MemoCreateScreen from './src/screens/MemoCreateScreen.jsx';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    // <MemoEditScreen />
-    <MemoCreateScreen />
+    <MemoEditScreen />
+    // <MemoCreateScreen />
   );
 }
 
