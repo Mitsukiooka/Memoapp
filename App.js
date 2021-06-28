7b6c0679d0c0bc3d50ab44f15f753183d6589f3d
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-06-28 15:43:28 +0900

    implement signup and login

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen.jsx';
 import MemoDetailScreen from './src/screens/MemoDetailScreen.jsx';
@@ -9,7 +10,13 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen.jsx';
 import LogInScreen from './src/screens/LogInScreen.jsx';
 import SignUpScreen from './src/screens/SignUpScreen';
 
+import { firebaseConfig } from './env';
+
 const Stack = createStackNavigator();
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+};
 
 export default function App() {
   return (
